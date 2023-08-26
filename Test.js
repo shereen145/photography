a9cbec1cbbc2dd0b4e8e87a17506f3cd9a0b3eba
shereen154 <shereen113642@miuegypt.edu.eg>
2023-08-26 10:05:34 +0300

    commit message

---
+++ Test.js
@@ -0,0 +1,29 @@
+alert("hello world");
+
+function getaverage (a,b){
+    var average= (a + b)/2
+    console.log(average);
+    return average;
+}
+var myResult= getaverage(7,8);
+console.log("the average is "+myResult);
+
+var myAge = 25;
+
+if (myAge< 18 || myAge >30 ) {
+
+    document.write("you can come , you are cool");
+} else {
+    document.write("you can't come !")
+}
+var a ="6"
+var b =5
+console.log (a * b)
+
+if (isNaN(a)) {
+
+    document.write("it isnot a number")
+    
+} else {
+    document.write("good")
+}
